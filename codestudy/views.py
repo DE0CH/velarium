6ff4e0247327e8b93001d0f76075b44dfbe05ba0
DE0CH <chendeyao000@gmail.com>
2021-01-15 20:46:54 +0800

    added footer

--- codestudy/views.py
+++ codestudy/views.py
@@ -13,9 +13,9 @@ from django.conf import settings
 from utils import s3_client
 import google.oauth2.id_token
 import google.auth.transport.requests
-from django.core.exceptions import PermissionDenied
 from .search_engine import search as s_search
 import enum
+from traceback import print_exc
 
 
 def get_base_context(request):
