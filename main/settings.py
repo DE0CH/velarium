95105efeb3738348e34be965e40a304b9b44d407
Deyao Chen <chendeyao000@gmail.com>
2022-01-16 20:23:49 +0800

    update google client id

--- main/settings.py
+++ main/settings.py
@@ -129,7 +129,7 @@ AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_EXPIRE = 300
 AWS_S3_REGION_NAME = 'ap-east-1'
 
-G_CLIENT_ID = '330093561618-bc6pprilkfqi5sc6oibm376psnfe9tub.apps.googleusercontent.com'
+G_CLIENT_ID = '370069200832-qho22j9ne287aos6cpchp6tnamh5gc5j.apps.googleusercontent.com'
 
 DEFAULT_FILE_STORAGE = 'main.storage_backends.MediaStorage'
 # Static files (CSS, JavaScript, Images)
